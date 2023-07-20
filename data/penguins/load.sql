59b3ac1d4991f3ed9ec01b0822aaa3dc55dbbe18
RandomFractals <RandomFractals@users.noreply.github.com>
2023-07-20 10:42:30 -0500

    add penguins.ipynb and create penguins.duckdb v0.81 for the demo duckdb data tables notebook, OJS, OMD and Quarto markdown docs (#10)

---
+++ data/penguins/load.sql
@@ -0,0 +1,1 @@
+COPY penguins FROM './penguins\penguins.csv' (FORMAT 'csv', header 0, delimiter ',', quote '"');
---
+++ data/penguins/schema.sql
@@ -0,0 +1,8 @@
+
+
+
+CREATE TABLE penguins(species VARCHAR, island VARCHAR, bill_length_mm FLOAT, bill_depth_mm FLOAT, flipper_length_mm INTEGER, body_mass_g FLOAT, sex VARCHAR);
+
+
+
+
